8f1980acdd6216d3d72072cd43b23c99b5f52d82
tamasstephen <kicsitnagyon@gmail.com>
2024-10-23 18:20:47 +0200

    feat: add welcome

---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/Button";
+import styles from "./Welcome.module.scss";
+
+export const Welcome = () => {
+  return (
+    <section className={styles.welcome}>
+      <div className={styles.content}>
+        <div className={styles["text-wrapper"]}>
+          <h2>Üdvözlöm a Comnica Signature-ben!</h2>
+          <p>
+            A következőkben végigvezetjük dokumentumai elfogadásán és aláírásán.
+            A folyamat több percet is igénybe vehet, ezért kérjük, csak akkor
+            kezdje el, ha készen áll rá.
+          </p>
+        </div>
+        <Button>Kezdhetjük</Button>
+      </div>
+    </section>
+  );
+};
